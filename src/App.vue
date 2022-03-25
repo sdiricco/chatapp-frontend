05658753a92bdbb6cdccdffbade3609ff54feb06
sdiricco-move <96438521+sdiricco-move@users.noreply.github.com>
2022-03-25 19:58:16 +0100

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -121,6 +121,14 @@ export default {
 </script>
 
 <style>
+
+body {
+  min-height: 100vh;
+  min-height: -webkit-fill-available;
+}
+html {
+  height: -webkit-fill-available;
+}
 /* width */
 ::-webkit-scrollbar {
   width: 10px;
